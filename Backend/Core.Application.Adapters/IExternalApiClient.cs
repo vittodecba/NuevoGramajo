dab0aa0167abf6d442d6845378e4f6e8e772fe30
vitto <vittomoyano@gmail.com>
2025-08-29 22:12:55 -0300

    se hizo lo del puerto
    cambio de ruta del http adapters

---
+++ Backend/Core.Application.Adapters/IExternalApiClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Adapters.Http
+{
+    
+    
+
+        
+    
+        public interface IExternalApiClient
+        {
+            Task<string> GetAsync(string url, Tuple<string, string>? authentication = null, IDictionary<string, string>? headers = null);
+            Task<HttpResponseMessage> PostAsync<T>(string uri, T item, Tuple<string, string>? authentication = null, IDictionary<string, string>? headers = null);
+            Task<HttpResponseMessage> PutAsync<T>(string url, T item, Tuple<string, string>? authentication = null, IDictionary<string, string>? headers = null);
+            Task<HttpResponseMessage> DeleteAsync(string url, Tuple<string, string>? authentication = null, IDictionary<string, string>? headers = null);
+        }
+    
+}
+
---
+++ Backend/Template-API/Controllers/AlumnoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Template-API.Controllers
+{
+    public class Alumno : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+}
